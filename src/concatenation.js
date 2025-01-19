39ba192788b67c6e4af75fef7f8fa32b0a413f51
oleksandrcore <153992542+oleksandrcore@users.noreply.github.com>
2025-01-19 12:52:17 +0200

    Initial commit

---
+++ src/concatenation.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.concatenation = concatenation;
+function concatenation(firstWord, secondWord) {
+    console.log(`${firstWord} ${secondWord}`);
+}
